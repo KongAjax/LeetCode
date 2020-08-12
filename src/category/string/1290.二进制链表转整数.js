fad81f84088b5e37bd862f292e6c59ea3d33d666
kongdesheng <kongdesheng@focusmedia.cn>
2020-08-12 10:36:42 +0800

    29 237 1290

---
+++ src/category/string/1290.二进制链表转整数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=1290 lang=javascript
+ *
+ * [1290] 二进制链表转整数
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number}
+ */
+var getDecimalValue = function(head) {
+  let number = '';
+  while(head){
+      number += head.val
+      head = head.next;
+  }
+  return  parseInt(number,2)
+};
+
+// @lc code=end
+
